eab3ca96436f9a7d0235a1fc1854108ff5038b18
Cameron Hatch <cameronlavigne92@gmail.com>
2018-08-20 13:59:49 -0500

    added separate function to calculate year of birth

--- funcDrills1.js
+++ funcDrills1.js
@@ -1,6 +1,9 @@
 'use strict';
+function getYearOfBirth(age){
+    return 2018-age;
+}
 function createGreeting(name,age){
-    const yearOfBirth = 2018-age;
+    const yearOfBirth = getYearOfBirth(age);
   return (`Hi my name is ${name}, and I'm ${age} years old. I was born in the year ${yearOfBirth} `);
 }
 

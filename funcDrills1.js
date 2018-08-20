5563cba777de43044633730b1bdd3facfcde6303
Cameron Hatch <cameronlavigne92@gmail.com>
2018-08-20 14:34:34 -0500

    added error for undefined arguments

--- funcDrills1.js
+++ funcDrills1.js
@@ -9,6 +9,9 @@ function getYearOfBirth(age){
 
 
 function createGreeting(name,age){
+  if (typeof name === "undefined" || typeof age === "undefined"){
+      throw new Error ('Arguments not valid');
+  } 
   const yearOfBirth = getYearOfBirth(age);
   return (`Hi my name is ${name}, and I'm ${age} years old. I was born in the year ${yearOfBirth} `);
 }
@@ -17,7 +20,7 @@ const createGreeting1 = createGreeting("Janet", 29);
 console.log(createGreeting1);
 
 try {
-  createGreeting("janet",-5);
+  createGreeting("Janet");
 } catch (e) {
   console.error(e.message);
 }

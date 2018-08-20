b9a1f3972cb4aa622496ef5c32ee88aee732a470
Janet Leon <leon.irenejanet@gmail.com>
2018-08-20 12:24:13 -0700

    modified function to throw errors

--- funcDrills1.js
+++ funcDrills1.js
@@ -1,11 +1,23 @@
 'use strict';
 function getYearOfBirth(age){
-    return 2018-age;
+  if(age < 0){
+    throw new Error ('Age cannot be negative'); 
+  }
+  
+  return 2018-age;
 }
+
+
 function createGreeting(name,age){
-    const yearOfBirth = getYearOfBirth(age);
+  const yearOfBirth = getYearOfBirth(age);
   return (`Hi my name is ${name}, and I'm ${age} years old. I was born in the year ${yearOfBirth} `);
 }
 
-const createGreeating1 = createGreeting("Janet", 29);
-console.log(createGreeating1);
+const createGreeting1 = createGreeting("Janet", 29);
+console.log(createGreeting1);
+
+try {
+  createGreeting("janet",-5);
+} catch (e) {
+  console.error(e.message);
+}

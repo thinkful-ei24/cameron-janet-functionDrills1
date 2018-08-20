a99410f6f315a8209bb5cb656514dcb4990e8537
Cameron Hatch <cameronlavigne92@gmail.com>
2018-08-20 13:46:34 -0500

    added function createGreeting

--- funcDrills1.js
+++ funcDrills1.js
@@ -1,3 +1,6 @@
 'use strict';
+function createGreeting(){
+    console.log('Hi my name is Janet, and I\'m 29 years old.');
+}
 
-console.log('Hi my name is Janet, and I\'m 29 years old.');
+createGreeting();
